b2533a0d98c08738c5c9256807868331dcd8a7fe
Alex Sindalovsky <alex@tinytap.it>
2023-03-16 15:43:20 +0200

    Init EDC repository
    fixed configurations

---
+++ contracts/EDuCoinMock.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {IForwarderRegistry} from "@animoca/ethereum-contracts/contracts/metatx/interfaces/IForwarderRegistry.sol";
+import {ForwarderRegistryContextBase} from "@animoca/ethereum-contracts/contracts/metatx/base/ForwarderRegistryContextBase.sol";
+import {ERC20Mintable} from "@animoca/ethereum-contracts/contracts/token/ERC20/ERC20Mintable.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {EDuCoin} from "./EDuCoin.sol";
+
+contract EDuCoinMock is EDuCoin, ERC20Mintable {
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint8 tokenDecimals,
+        address[] memory recipients,
+        uint256[] memory amounts,
+        IForwarderRegistry forwarderRegistry
+    ) EDuCoin(tokenName, tokenSymbol, tokenDecimals, recipients, amounts, forwarderRegistry) {}
+
+    /// @inheritdoc EDuCoin
+    function _msgSender() internal view virtual override(Context, EDuCoin) returns (address) {
+        return EDuCoin._msgSender();
+    }
+
+    /// @inheritdoc EDuCoin
+    function _msgData() internal view virtual override(Context, EDuCoin) returns (bytes calldata) {
+        return EDuCoin._msgData();
+    }
+
+    function __msgData() external view returns (bytes calldata) {
+        return _msgData();
+    }
+}
---
+++ hardhat-config/external.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  external: {
+    contracts: [
+      {
+        artifacts: './node_modules/@animoca/ethereum-contracts/artifacts',
+      },
+    ],
+  },
+}
+
