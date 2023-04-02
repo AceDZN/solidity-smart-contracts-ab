f19cc7db566d1f48c45a80d8f83e0648f012875d
Alex Sindalovsky <alex@tinytap.it>
2023-04-02 12:43:41 +0300

    deploy script is based on amedAccounts + ethereum-migrations

---
+++ deploy/01_edc_deploy.js
@@ -0,0 +1,100 @@
+const { utils } = require('ethers')
+const { skipChainTypesExceptFor } = require('@animoca/ethereum-migrations/src/helpers/common')
+const Contract_deploy = require('@animoca/ethereum-migrations/src/templates/Contract/deploy')
+
+async function getAllocations(hre) {
+  const namedAccounts = await hre.getNamedAccounts()
+
+  const mainnetAllocations = [
+    {
+      name: 'LAUNCHPAD_PUBLIC_SALE_WALLET',
+      address: namedAccounts.LAUNCHPAD_PUBLIC_SALE_WALLET,
+      amount: '900000000',
+    },
+    {
+      name: 'LIQUIDITY_POOL_WALLET',
+      address: namedAccounts.LIQUIDITY_POOL_WALLET,
+      amount: '900000000',
+    },
+    {
+      name: 'EARLY_CONTRIBUTOR_POOL_WALLET',
+      address: namedAccounts.EARLY_CONTRIBUTOR_POOL_WALLET,
+      amount: '300000000',
+    },
+    {
+      name: 'ECOSYSTEM_FUND_WALLET',
+      address: namedAccounts.ECOSYSTEM_FUND_WALLET,
+      amount: '200000000',
+    },
+    {
+      name: 'TREASURY_WALLET',
+      address: namedAccounts.TREASURY_WALLET,
+      amount: '273946700',
+    },
+    {
+      name: 'STRATEGIC_INVESTORS_WALLET',
+      address: namedAccounts.STRATEGIC_INVESTORS_WALLET,
+      amount: '6053300',
+    },
+    {
+      name: 'ADVISORS_WALLET',
+      address: namedAccounts.ADVISORS_WALLET,
+      amount: '20000000',
+    },
+    {
+      name: 'MARKETING_WALLET',
+      address: namedAccounts.MARKETING_WALLET,
+      amount: '400000000',
+    },
+    {
+      name: 'TEAM_WALLET',
+      address: namedAccounts.TEAM_WALLET,
+      amount: '400000000',
+    },
+  ]
+
+  const testnetAllocations = [
+    {
+      name: 'Deployer',
+      address: namedAccounts.deployer,
+      amount: '2000000000',
+    },
+    ...mainnetAllocations,
+  ]
+
+  const allocations = hre.network.tags.production ? mainnetAllocations : testnetAllocations
+  return allocations
+}
+
+module.exports = Contract_deploy('EDuCoinz', {
+  contract: 'EDuCoin',
+  args: [
+    {
+      name: 'tokenName',
+      value: 'EDUCointest',
+    },
+    {
+      name: 'tokenSymbol',
+      value: 'EDUT',
+    },
+
+    {
+      name: 'tokenDecimals',
+      value: 18,
+    },
+    {
+      name: 'holders',
+      value: async (hre) => (await getAllocations(hre)).map((allocation) => allocation.address),
+    },
+    {
+      name: 'amounts',
+      value: async (hre) => (await getAllocations(hre)).map((allocation) => utils.parseEther(allocation.amount)),
+    },
+
+    {
+      name: 'forwarderRegistry',
+      value: '0xB87EbEB1f4aA317bd3eEc04704D3fFD6e3BC4b8f',
+    },
+  ],
+})
+module.exports.skip = skipChainTypesExceptFor('bsc')
--- hardhat-config/networks.config.js
+++ hardhat-config/networks.config.js
@@ -1,56 +1,49 @@
 const os = require('os').homedir()
-const { mnemonic } = require(os + '/.mnemonics')
+//const { mnemonic } = require(os + '/.mnemonics')
 module.exports = {
-  defaultNetwork: "bscTestnet",
+  defaultNetwork: 'bscTestnet',
   networks: {
     hardhat: {
       gas: 19000000,
       allowUnlimitedContractSize: true,
       timeout: 1800000,
-      accounts: { mnemonic },
+      tags: ['testnet', 'hardhat'],
     },
-
     mainnet: {
       url: process.env.PROVIDER_URL_INFURA_MAINNET,
       chainId: 1,
       gasPrice: 'auto',
-      tags: ['production'],
-      accounts: { mnemonic },
-    },
-    polygon: {
-      url: process.env.PROVIDER_URL_INFURA_MATIC,
-      chainId: 137,
-      gasPrice: 'auto',
-      tags: ['production'],
-      accounts: { mnemonic },
+      tags: ['production', 'ethereum'],
     },
     goerli: {
       url: process.env.PROVIDER_URL_INFURA_GOERLI,
       chainId: 5,
       gasPrice: 'auto',
-      tags: ['testnet'],
-      accounts: { mnemonic },
+      tags: ['testnet', 'ethereum'],
+    },
+    polygon: {
+      url: process.env.PROVIDER_URL_INFURA_MATIC,
+      chainId: 137,
+      gasPrice: 'auto',
+      tags: ['production', 'polygon'],
     },
     mumbai: {
       url: process.env.PROVIDER_URL_INFURA_MUMBAI,
       chainId: 80001,
       gasPrice: 'auto',
-      tags: ['testnet'],
-      accounts: { mnemonic },
+      tags: ['testnet', 'polygon'],
     },
     bsc: {
       url: process.env.PROVIDER_URL_INFURA_BSC_MAINNET,
       chainId: 56,
       gasPrice: 'auto',
-      tags: ['production'],
-      accounts: { mnemonic },
+      tags: ['production', 'bsc'],
     },
     bscTestnet: {
       url: process.env.PROVIDER_URL_INFURA_BSC_TESTNET,
       chainId: 97,
       gasPrice: 'auto',
-      tags: ['testnet'],
-      accounts: { mnemonic },
+      tags: ['testnet', 'bsc'],
     },
   },
 }
